20528c2bbdece595c5f7b19f9fb0dc937955caf6
JoshASC4 <jmnunez051@gmail.com>
2017-07-11 11:58:47 -0400

    first commit

---
+++ pokemonTeam.js
@@ -0,0 +1,7 @@
+
+var pokemonRoster = ["Mewtwo", "Giratina", "Arceus", "Zapdos", "Regigigas", "Dialga"];
+
+for (var i=0; i<6; i++)
+{
+    console.log(pokemonRoster[i]);
+}
---
+++ pokemonVariable.js
@@ -0,0 +1,10 @@
+var rayquazaHP = 320;
+var rayquazaNum = 209;
+var rayquazaType1 = "Dragon";
+var rayquazaType2 = "Flying";
+var rayquazaBestMove = "Dragon Pulse";
+var rayquazaLastEvo = false;
+var rayquazaGender = "None"
+
+
+rayquazaHP = rayquazaHP-10
